e3f9ba472eba4deee590262aa76f5ad8864aaa76
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2022-06-21 13:07:44 +0530

    Just An Minor Update

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/TechnoMindz/CommonBot_For_All.git /CommonBot_For_All
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /CommonBot_For_All
 fi
-cd /EvaMaria
+cd /ExtraFeatureBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
