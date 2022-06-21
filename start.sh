3f812c4a747fcd4e4d0d9191d46700cd26fef855
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2022-06-21 13:17:45 +0530

    Working Update

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
-echo "Cloning Repo, Please Wait..."
-git clone -b main https://github.com/TechnoMindz/CommonBot_For_All.git /CommonBot_For_All
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/TechnoMindz/CommonBot_For_All.git /CommonBot_For_All
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /CommonBot_For_All
+fi
 cd /CommonBot_For_All
-echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
 python3 bot.py
