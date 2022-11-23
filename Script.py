02b568b222ab41fc2f4ac08139196d76866bbeee
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2022-11-23 09:02:26 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -16,7 +16,7 @@ class script(object):
 - Source - <a href=https://github.com/TechnoMindz/CommonBot_For_All><b><i>Techno Mindz</i></b></a>  
 
 <b>DEVS:</b>
-- <a href=https://t.me/TechnoKillerbot><b><i>★彡[ʀᴀᴠɪɴᴀɴᴅᴀɴ]彡★</i></b></a>"""
+- <a href=https://t.me/TechnoMindzYt><b><i>★彡[ʀᴀᴠɪɴᴀɴᴅᴀɴ]彡★</i></b></a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and Techno will respond whenever a keyword is found the message
@@ -41,7 +41,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/technokillerbot)</code>
+<code>[Button Text](buttonurl:https://t.me/tmmainchannel)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
