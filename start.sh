e405c7778b772873ddc76c7dbe3f6b141b589b45
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2023-05-01 16:36:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/RaviTechnoMindz/AutoFilter.git /AutoFilter
+  git clone https://github.com/TechnoMindz/CommonBot_For_All.git /CommonBot_For_All
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoFilter
+  git clone $UPSTREAM_REPO /CommonBot_For_All
 fi
-cd /AutoFilter
+cd /CommonBot_For_All
 pip3 install -U -r requirements.txt
 echo "Starting Techno Mindz ❤️‍🔥"
 python3 bot.py
